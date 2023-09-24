9561d79179bd600576b943c7531b86e19ded6949
Bhargavi Mopuru <106804698+MBhargavi235@users.noreply.github.com>
2023-09-24 19:13:16 +0530

    Update index.js

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -18,3 +18,26 @@ const Item = props => {
 }
 
 export default Item
+
+
+/*
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const Item = props => {
+  const {details} = props
+  const {id, name, logoUrl} = details
+  return (
+    <li>
+      <Link to={`/courses/${id}`} className="link-el">
+        <img src={logoUrl} alt={name} />
+        <p>{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default Item
+
+*/
