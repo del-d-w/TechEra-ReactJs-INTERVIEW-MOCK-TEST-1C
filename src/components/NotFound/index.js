444daef8e33ca92b3e799a72e38a4385e223a8ba
Bhargavi Mopuru <106804698+MBhargavi235@users.noreply.github.com>
2023-09-24 19:14:41 +0530

    Update index.js

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -23,3 +23,31 @@ const NotFound = () => (
 )
 
 export default NotFound
+
+/*
+import {Link} from 'react-router-dom'
+
+const NotFound = () => (
+  <div>
+    <Link to="/" className="link-el">
+      <navbar>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/tech-era/website-logo-img.png"
+          alt="website logo"
+        />
+      </navbar>
+    </Link>
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/tech-era/not-found-img.png"
+        alt="not found"
+      />
+      <h1>Page Not Found</h1>
+      <p>We are sorry, the page you requested could not be found</p>
+    </div>
+  </div>
+)
+
+export default NotFound
+
+*/
